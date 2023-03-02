7fcdd0bb5f346d185e08c4fdccaa92cafe876e01
Gentlemannerss <damonvanbergen@gmail.com>
2023-03-03 00:08:21 +0100

    huiswerk + bonus

--- src/Translator.java
+++ src/Translator.java
@@ -1,6 +1,20 @@
 import java.util.HashMap;
 
 public class Translator {
-    HashMap<Integer Main.numeric, String> numericAlpha = new HashMap();
-    numericAlpha.put(numeric[], alphabetic[]);
+    HashMap<Integer, String> numericAlpha = new HashMap();
+
+    public Translator(Integer[] numeric, String[] alphabetic) {
+        for (int i = 0; i < numeric.length; i++) {
+            numericAlpha.put(numeric[i], alphabetic[i]);
+        }
+    }
+
+    public String translate(Integer number) {
+        String alphabetic = numericAlpha.get(number);
+        if (alphabetic == null) {
+            return "Dit cijfer is te groot.";
+        } else {
+            return alphabetic;
+        }
+    }
 }
---
+++ src/randomNumber.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Random;
+public class randomNumber {
+    public static HashSet<Integer> randomNumberGenerator() {
+        HashSet<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+
+        while (numbers.size() < 4) {
+            int randomNumber = random.nextInt(10) + 1; // generates a random number between 1 and 10
+            numbers.add(randomNumber);
+        }
+
+        return numbers;
+    }
+
+    public static String setToString(HashSet<Integer> set) {
+        StringBuilder sb = new StringBuilder();
+        for (Integer num : set) {
+            sb.append(num);
+        }
+        return sb.toString();
+    }
+}
